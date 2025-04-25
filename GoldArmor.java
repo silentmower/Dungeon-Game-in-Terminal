0619c0c4a2309ec76bf71bc62b8f26687d190c01
Filip <106113670+silentmower@users.noreply.github.com>
2025-04-26 00:03:07 +0200

    Delete GoldArmor.java

--- GoldArmor.java
+++
@@ -1,8 +0,0 @@
-// Klasa reprezentująca "Gold Armor" – zbroję blokującą 25% obrażeń
-public class GoldArmor extends Armor {
-    // Konstruktor tworzy zbroję typu GoldArmor
-    public GoldArmor() {
-        // id: "armor2", nazwa: "Gold Armor", wartość: 0 (lub inna), blokowanie: 25%
-        super("armor2", "Gold Armor", 0, 25);
-    }
-}
