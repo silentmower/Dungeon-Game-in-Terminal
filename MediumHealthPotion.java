6d538c37a2c3069517961429c697c9b130bbd7f3
Filip <106113670+silentmower@users.noreply.github.com>
2025-04-26 00:03:41 +0200

    Delete MediumHealthPotion.java

--- MediumHealthPotion.java
+++
@@ -1,6 +0,0 @@
-// Klasa reprezentuje średnią miksturę zdrowia, która leczy 30 punktów zdrowia
-public class MediumHealthPotion extends Potion {
-    public MediumHealthPotion() {
-        super("potion2", "Medium Health Potion", 30);
-    }
-}
