61338151adfb3d07db558c62a3bb5695a1cf2928
Filip <106113670+silentmower@users.noreply.github.com>
2025-04-26 00:03:20 +0200

    Delete LargeHealthPotion.java

--- LargeHealthPotion.java
+++
@@ -1,6 +0,0 @@
-// Klasa reprezentuje dużą miksturę zdrowia, która leczy 50 punktów zdrowia
-public class LargeHealthPotion extends Potion {
-    public LargeHealthPotion() {
-        super("potion3", "Large Health Potion", 50);
-    }
-}
